aac40fbcc78d421d79ac73916561c9d749780c6a
Olivia Mack-Solden <omacksolden@gmail.com>
2016-06-08 19:51:55 -0400

    Include array of objects about presidents

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
 
 var iceCream = ['Moose Tracks', 'Half Baked', 'Cookie Dough',
 'Cookies and Cream'];
+
+var presidents = [{
+	firstName: 'Barack',
+	lastName: 'Obama',
+	termLength: '8 years',
+	party: 'Democratic',
+	yearsOfPresidency: '2009-2016'
+}, {
+	firstName: 'George W.',
+	lastName: 'Bush',
+	termLength: '8 years',
+	party: 'Republican',
+	yearsOfPresidency: '2001-2009'
+}, {
+	firstName: 'Bill',
+	lastName: 'Clinton',
+	termLength: '8 years',
+	party: 'Democratic',
+	yearsOfPresidency: '1993-2001'
+}, {
+	firstName: 'George H. W.',
+	lastName: 'Bush',
+	termLength: '4 years',
+	party: 'Republican',
+	yearsOfPresidency: '1989-1993'
+}, {
+	firstName: 'Ronald',
+	lastName: 'Reagan',
+	termLength: '8 years',
+	party: 'Republican',
+	yearsOfPresidency: '1981-1989'
+}];
+
+console.log(presidents[2].firstName + ' ' + presidents[2].lastName);

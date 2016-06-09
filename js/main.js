357994c61c4cb85ce662def0cdf69346a7239656
Olivia Mack-Solden <omacksolden@gmail.com>
2016-06-08 21:03:25 -0400

    Add variable numba function expression

--- js/main.js
+++ js/main.js
@@ -41,3 +41,17 @@ function howdy() {
 }
 
 console.log(howdy());
+
+var numba = function (string) {
+	if (string.length < 7) {
+		console.log('What a short little word!');
+	} else if (string.length > 7) {
+		console.log('I’m sorry, but that’s too many to count.');
+	} else {
+		console.log('7, what a perfect choice!');
+	}
+};
+
+numba('7');
+numba('seventy');
+numba('university');
